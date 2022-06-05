1ec4f0ec9821389f4be5f5a9f6c470161c0ca45e
stickfiregames <98129819+stickfiregames@users.noreply.github.com>
2022-06-05 12:59:41 +0100

    add blue themes

--- resources/themes.js
+++ resources/themes.js
@@ -1,23 +1,25 @@
 var HTML = document.documentElement,
-    current_theme =
-        localStorage.getItem("theme") ||
-        ((window.matchMedia("(prefers-color-scheme: dark)") &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches) ? "dark" : "default"),
-    themes = [
-        "default",
-        "dark"
-    ],
-    theme_button = $("#theme_button"),
-    set_theme = (theme) => {
-        current_theme = theme;
-        localStorage.setItem("theme", theme);
-        HTML.className = theme;
-    };
+  current_theme =
+    localStorage.getItem("forumoji-theme") || // local storage is shared across lopste.github.io, so avoid conflicts just in case
+    ((window.matchMedia("(prefers-color-scheme: dark)") &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches) ? "dark" : "default"),
+  themes = [
+    "default",
+    "dark",
+    "blue",
+    "blue-dark"
+  ],
+  theme_button = $("#theme_button"),
+  set_theme = (theme) => {
+    current_theme = theme;
+    localStorage.setItem("forumoji-theme", theme);
+    HTML.className = theme;
+  };
 
 theme_button.click(function switch_theme() {
-    let next_index = themes.indexOf(current_theme) + 1;
-    if (next_index >= themes.length)
-        return set_theme(themes[0]);
-    set_theme(themes[next_index]);
+  let next_index = themes.indexOf(current_theme) + 1;
+  if (next_index >= themes.length)
+    return set_theme(themes[0]);
+  set_theme(themes[next_index]);
 })
 set_theme(current_theme);
