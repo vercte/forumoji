2e1480b8f05df9ee6af21534002d33eba6ea5924
Vercte <lopstedener@gmail.com>
2024-01-01 22:47:59 -0800

    cmon windows do you really have to do me like that

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import InfoTray from "components/Sidebar";
 import List from "src/components/List/List";
 import { EmojiData } from "./EmojiList";
 
-import "css/App.css";
+import "css/app.css";
 
 export default function App() {
     const [emoji, setEmoji] = React.useState<EmojiData>();
